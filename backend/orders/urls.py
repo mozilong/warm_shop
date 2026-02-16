4993a372dabfdaf116f882709429f6e28f3be28a
mozilong <mozilong668@126.com>
2026-02-16 14:39:26 +0800

    first commit

--- backend/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views  # 改为导入整个views模块，避免指定不存在的类
-
-urlpatterns = [
-    path('create/', views.order_create, name='order_create'),  # 创建订单
-    path('', views.order_list, name='order_list'),            # 订单列表
-    path('express/<int:order_id>/', views.order_express, name='order_express'),  # 物流信息
-]
--- backend/warm_shop/__init__.py
+++
@@ -1,3 +0,0 @@
-import pymysql
-# 将pymysql伪装成MySQLdb，适配Django的MySQL驱动检测
-pymysql.install_as_MySQLdb()
